55d5212aa8a8f78dff43d80162e80e38ccf0bc01
deepak <newdeep12@gmail.com>
2019-10-13 14:37:38 +0530

    hub.component.ts

--- src/app/hub/hub.component.ts
+++ src/app/hub/hub.component.ts
@@ -15,7 +15,7 @@ private loadAmount : any;
     this.loadAmount = JSON.parse(localStorage.getItem('loanAmount'));
   }
 
-  private getInterestRate(event){
+  public getInterestRate(event){
     this.getInterestAmout = event;
     this.loadAmount = JSON.parse(localStorage.getItem('loanAmount'));
   }
